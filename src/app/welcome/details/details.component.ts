b6c8c0724480f576b4d5c54156182d98f2f4b853
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-18 12:57:53 +0300

    347. Building a First Standalone Component

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import {SharedModule} from "../../shared/shared.module";
 
 @Component({
+  standalone: true,
+  imports: [SharedModule],
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css'],
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
@@ -13,3 +16,4 @@ export class DetailsComponent {
     this.analyticsService.registerClick();
   }
 }
+
