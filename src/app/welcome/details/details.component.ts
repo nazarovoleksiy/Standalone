d5c3b82b47c0ff09344ad1880e68ab9ecadb2f56
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-18 13:10:12 +0300

    350. Migrating Another Component

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { AnalyticsService } from 'src/app/shared/analytics.service';
-import {SharedModule} from "../../shared/shared.module";
+import {Component} from '@angular/core';
+
+import {AnalyticsService} from 'src/app/shared/analytics.service';
+import {HighlightDirective} from "../../shared/highlight.directive";
 
 @Component({
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighlightDirective],
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent {
-  constructor(private analyticsService: AnalyticsService) {}
+  constructor(private analyticsService: AnalyticsService) {
+  }
 
   onClick() {
     this.analyticsService.registerClick();
